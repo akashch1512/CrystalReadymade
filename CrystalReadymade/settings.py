ffb1fb31001cba304d644aecaef3e101d9c5139a
akash <akashchaudhari1012@gmail.com>
2024-11-30 16:45:48 +0530

    hosting

--- CrystalReadymade/settings.py
+++ CrystalReadymade/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(!xca-m4fwlz8nqvw@_s52*hd1xu753=k_i^a%o7=&ef2*z0x_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
